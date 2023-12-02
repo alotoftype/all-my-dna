2f5ca1cb607a5f93b0ba6f17057524700f9949df
Serge M <alotoftype@users.noreply.github.com>
2023-12-02 12:26:54 -0500

    adding custom favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "All My DNA",
   description: "All My DNA",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
