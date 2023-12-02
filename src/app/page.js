cefcd3ada5ef4d8095f581dbcb6752d5b9db9374
Serge M <alotoftype@users.noreply.github.com>
2023-12-02 12:16:53 -0500

    switching to svg assets

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,13 @@ export default function Home() {
     <main className={styles.main}>
       <Image
         srcSet={
-          "/mobile_lockup@2x.jpg 640w, " + // for screens up to 640px wide
+          "/mobile_lockup@1.5x.svg 640w, " + // for screens up to 640px wide
           "/Artboard@1x.jpg 1920w" // for screens wider than 640px
         }
-        src="/Artboard@2x.jpg" // default src, for browsers that don't support srcSet
+        src="/Artboard@1x.svg" // default src, for browsers that don't support srcSet
         alt="All My DNA"
-        width={536}
-        height={386}
+        width={100}
+        height={50}
         layout="responsive"
       />
       <p> Copyright ©️ 2023 All My DNA, LLC - All Rights Reserved.</p>
