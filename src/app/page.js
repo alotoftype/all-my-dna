d3e87c31ac8a8cc2e88c579b2f5359a08535ab03
Serge M <alotoftype@users.noreply.github.com>
2023-12-03 16:37:00 -0500

    changing scale.

--- src/app/page.js
+++ src/app/page.js
@@ -4,18 +4,23 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Image
-        srcSet={
-          "/mobile_lockup@1.5x.svg 640w, " + // for screens up to 640px wide
-          "/Artboard@1x.jpg 1920w" // for screens wider than 640px
-        }
-        src="/Artboard@1x.svg" // default src, for browsers that don't support srcSet
-        alt="All My DNA"
-        width={100}
-        height={50}
-        layout="responsive"
-      />
-      <p> Copyright &copy; {new Date().getFullYear()} All My DNA, LLC - All Rights Reserved.</p>
+      <div className={styles.logo}>
+        <Image
+          srcSet={
+            "/mobile_lockup@1.5x.svg 640w, " + // for screens up to 640px wide
+            "/Desktop_logo.svg 1920w" // for screens wider than 640px
+          }
+          src="/Desktop_logo.svg" // default src, for browsers that don't support srcSet
+          alt="All My DNA"
+          width={207}
+          height={100}
+          layout="responsive"
+        />
+      </div>
+      <p>
+        Copyright &copy; {new Date().getFullYear()} All My DNA, LLC - All Rights
+        Reserved.
+      </p>
     </main>
   );
 }
